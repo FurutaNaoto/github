ef6bb1b5c2cef6091623ebd7e09282a307a7cf5c
FurutaNaoto <n70furuta@gmail.com>
2025-02-28 11:36:17 +0900

    Delete css directory

--- css/旧/footer.js
+++
@@ -1,6 +0,0 @@
-var footer_innerHTML = document.getElementById('footer');
-footer_innerHTML.innerHTML = `
-<div class="footers">
-Copyright (C) All Rights Reserved.
-</div>
-`;
--- css/旧/header.js
+++
@@ -1,9 +0,0 @@
-var header_innerHTML = document.getElementById('header');
-header_innerHTML.innerHTML = `
-<div class="headers">
-  <a target="_self" href="LispHelpHome.html"><!--UtHelpHome.htmlにする予定-->
-    <!--altの値を取得してcssフォルダパスと結合し、srcの値を書き換えている-->
-    <img class="img_Item" src='' alt='UTlogo.png'>
-  </a>
-</div>
-`;
